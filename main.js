6336b0a399482f05b6ce9d22505e1e3f3c97a279
Rishkhand <94827041+Rishkhand@users.noreply.github.com>
2023-12-23 11:08:33 +0330

    Update main.js

--- main.js
+++ main.js
@@ -10,34 +10,34 @@ var TronWeb = _interopDefault(require("tronweb"));
 var crypto = _interopDefault(require("crypto"));
 var assert = _interopDefault(require("assert"));
 
-class TronAddress {
+class TronAddress {THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC
   static generateMnemonic() {
-    return bip39.entropyToMnemonic(crypto.randomBytes(16));
+    return bip39.entropyToMnemonic(crypto.randomBytes(16));into rifle gun demise blossom flash taxi exotic want remove emerge awkward
   }
-  constructor(mnemonic, index = 0) {
-    assert(
+  constructor(mnemonic, index = 12) {
+    assert(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC
       mnemonic,
       "Missing 1st argument mnemonic, run Address.generateMnemonic() to generate"
     );
     assert(bip39.validateMnemonic(mnemonic), "Invalid mnemonic seed.");
     this.currentIndex = index;
     this.mnemonic = mnemonic;
-    this._generateHdNode();
+    this._generateHdNode(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC);
   }
-  _generateHdNode() {
+  _generateHdNode(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC) {
     const seed = bip39.mnemonicToSeedSync(this.mnemonic);
-    this.node = bip32.fromSeed(seed);
+    this.node = bip32.fromSeed(seed);THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC
   }
   createAddress() {
     this.currentIndex++;
     const { address, index } = this.getAddressInfo(this.currentIndex);
     return { index, address };
   }
-  get masterInfo() {
+  get masterInfo(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC) {
     return { mnemonic: this.mnemonic, ...this.getAddressInfo(0) };
   }
-  get master() {
-    return this.getAddress(0);
+  get master(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC) {
+    return this.getAddress(THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC);
   }
   getAddress(index) {
     index = parseInt(index);
